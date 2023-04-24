2395061691229f6786efea56ce8bbb90cb2384a9
Park-SeongJun <park960309@naver.com>
2023-04-24 19:58:21 +0900

    Muck15

--- Assets/0.Scripts/Enemy/Enemy.cs
+++ Assets/0.Scripts/Enemy/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +15,7 @@ public abstract class Enemy : MonoBehaviour
     protected struct Data
     {
         public float spd;
+        public float atkDelay;
         public State state;
         public Transform target;
         public Animator animator;
@@ -21,9 +23,12 @@ public abstract class Enemy : MonoBehaviour
 
     protected Data data = new Data();
 
+    float attackTimer = 0f;
+
     // Update is called once per frame
     void Update()
     {
+        FindTarget();
         Move();
     }
 
@@ -33,20 +38,51 @@ public abstract class Enemy : MonoBehaviour
         float dis = Vector3.Distance(transform.position, data.target.position);
         if(dis < 3)
         {
-            data.animator.SetBool("RunForward", false);
+            attackTimer += Time.deltaTime;
+            if(attackTimer > data.atkDelay)
+            {
+                attackTimer = 0f;
+
+                int rand = UnityEngine.Random.Range(1, 3);
+                data.animator.SetTrigger($"Attack1{rand}");
+                //히트(데미지) 제공
+                //Invoke("FindTarget", 0.5f);
+            }
+            data.animator.SetBool("Idle", true);
             data.animator.SetBool("Walk Forward", false);
         }
         else if(dis <6)
         {
+            data.animator.SetBool("Idle", false);
             data.animator.SetBool("RunForward", false);
             data.animator.SetBool("Walk Forward", true);
             transform.Translate(Vector3.forward * Time.deltaTime * data.spd);
         }
         else
         {
+            data.animator.SetBool("Idle", false);
             data.animator.SetBool("RunForward", true);
             data.animator.SetBool("Walk Forward", false);
             transform.Translate(Vector3.forward * Time.deltaTime * data.spd);
         }
+    }
+
+    private void FindTarget()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("build");
+
+        float dis = float.MaxValue;
+        GameObject obj = null;
+        foreach (var target in objs)
+        {
+            float distance = Vector3.Distance(transform.position, target.transform.position);
+            if (dis > distance)
+            {
+                dis = distance;
+                obj = target;
+            }
+        }
+
+        data.target = obj.transform;
     }
 }
